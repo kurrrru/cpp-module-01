5db10a2dac4192cc5f8fb78d4a623a3c4ac3270a
kurrrru <kurrrru8850@gmail.com>
2025-02-19 23:56:56 +0900

    update

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,18 +2,36 @@
 #include <iostream>
 #include <string>
 
+namespace {
+namespace color {
+    const char light[] = "\033[1m";
+    const char green[] = "\033[1;32m";
+    const char red[] = "\033[1;31m";
+    const char blue[] = "\033[1;34m";
+    const char reset[] = "\033[0m";
+}
+}  // namespace
+
 int main() {
     std::string str("HI THIS IS BRAIN");
     std::string *stringPTR = &str;
     std::string &stringREF = str;
 
-    std::cout << "== Address comparison ==" << std::endl;
-    std::cout << std::setw(13) << "str: " << &str << std::endl;
-    std::cout << std::setw(13) << "stringPTR: " << &stringPTR << std::endl;
-    std::cout << std::setw(13) << "stringREF: " << &stringREF << std::endl;
+    std::cout << color::light << "== Address comparison =="
+        << color::reset << std::endl;
+    std::cout << std::setw(13) << "str: " << color::green
+        << &str << color::reset << std::endl;
+    std::cout << std::setw(13) << "stringPTR: " << color::red
+        << stringPTR << color::reset << std::endl;
+    std::cout << std::setw(13) << "stringREF: " << color::blue
+        << &stringREF << color::reset << std::endl;
 
-    std::cout << "== Value comparison ==" << std::endl;
-    std::cout << std::setw(13) << "str: " << str << std::endl;
-    std::cout << std::setw(13) << "stringPTR: " << *stringPTR << std::endl;
-    std::cout << std::setw(13) << "stringREF: " << stringREF << std::endl;
+    std::cout << color::light << "== Value comparison =="
+        << color::reset << std::endl;
+    std::cout << std::setw(13) << "str: " << color::green
+        << str << color::reset << std::endl;
+    std::cout << std::setw(13) << "stringPTR: " << color::red
+        << *stringPTR << color::reset << std::endl;
+    std::cout << std::setw(13) << "stringREF: " << color::blue
+        << stringREF << color::reset << std::endl;
 }
